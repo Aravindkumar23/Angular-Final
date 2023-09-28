56545eff04db2899e11f3093eabef5f036891736
Aravindkumar23! <umaaravind1965@gmail.com>
2023-09-28 09:50:28 +0530

    Master

--- src/app/anime/anime.module.ts
+++ src/app/anime/anime.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { AnimeRoutingModule } from './anime-routing.module';
 import { PosterComponent } from './poster/poster.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { DisplayComponent } from './display/display.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AddAnimeComponent } from './add-anime/add-anime.component';
@@ -19,15 +19,18 @@ import {
   MatChipInputEvent,
   MatChipsModule,
 } from '@angular/material/chips';
-import{ MatDialog,
+import {
+  MatDialog,
   MAT_DIALOG_DATA,
   MatDialogRef,
-  MatDialogModule} from '@angular/material/dialog';
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { EditAnimeComponent } from './edit-anime/edit-anime.component';
 import { LikeDislikeComponent } from './like-dislike/like-dislike.component';
 import { MatBadgeModule } from '@angular/material/badge';
 import { DisplayDetailsComponent } from './display-details/display-details.component';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -38,12 +41,24 @@ import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
     LikeDislikeComponent,
     DisplayDetailsComponent,
     DeleteDialogComponent,
-   
   ],
   imports: [
     CommonModule,
-    AnimeRoutingModule,MatButtonModule,MatCardModule,MatIconModule,HttpClientModule,FormsModule,ReactiveFormsModule,
-    MatFormFieldModule,MatInputModule,MatRadioModule,MatSelectModule,MatChipsModule,MatBadgeModule,MatDialogModule,
-  ]
+    AnimeRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatDialogModule,
+    MatSnackBarModule,
+  ],
 })
-export class AnimeModule { }
+export class AnimeModule {}
