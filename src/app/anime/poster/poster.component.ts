07f95b5bf7c0f85639c1cf32a59c86fe1460bab9
Aravindkumar23! <umaaravind1965@gmail.com>
2023-10-03 11:23:23 +0530

    Genres done

--- src/app/anime/poster/poster.component.ts
+++ src/app/anime/poster/poster.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { anime } from 'src/app/app.component';
 import { DataService } from 'src/app/data.service';
@@ -10,93 +10,90 @@ import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component'
 @Component({
   selector: 'app-poster',
   templateUrl: './poster.component.html',
-  styleUrls: ['./poster.component.css']
+  styleUrls: ['./poster.component.css'],
 })
 export class PosterComponent {
-  @Output() removeanime= new EventEmitter();
-  @Input() Anime:anime
-    ={
-      id:'',
-      title: "",
+  @Output() removeanime = new EventEmitter();
+  @Input() Anime: anime = {
+    id: '',
+    title: '',
     rating: 0,
-    poster: "",
-    summary: "",
-    trailer: "",
+    poster: '',
+    summary: '',
+    trailer: '',
     like: 0,
     dislike: 0,
-    
-    censorRating: "",
+
+    censorRating: '',
     genres: [],
-    languages: []
-  }
-  Languages=LANGUAGES;
-  value:boolean = true;
+    languages: [],
+  };
+  Languages = LANGUAGES;
+  value: boolean = true;
   LikeSubject = new Subject<number>();
   DislikeSubject = new Subject<number>();
-  constructor(private router:Router,private data:DataService,private dialog:MatDialog){
-
-    this.LikeSubject.pipe(debounceTime(2000),
-    switchMap(
-      (count) => 
-      {this.Anime = {...this.Anime,like:count};
-      return this.data.editAnime(this.Anime);
-    })).subscribe();
-    this.DislikeSubject.pipe(debounceTime(2000),
-    switchMap(
-      (count) => 
-      {this.Anime = {...this.Anime,dislike:count};
-      return this.data.editAnime(this.Anime);
-    })).subscribe();
+  constructor(
+    private router: Router,
+    private data: DataService,
+    private dialog: MatDialog
+  ) {
+    this.LikeSubject.pipe(
+      debounceTime(2000),
+      switchMap((count) => {
+        this.Anime = { ...this.Anime, like: count };
+        return this.data.editAnime(this.Anime);
+      })
+    ).subscribe();
+    this.DislikeSubject.pipe(
+      debounceTime(2000),
+      switchMap((count) => {
+        this.Anime = { ...this.Anime, dislike: count };
+        return this.data.editAnime(this.Anime);
+      })
+    ).subscribe();
   }
 
-  viewEdit(){
-    this.router.navigate(['/anime/edit',this.Anime.id]);
+  viewEdit() {
+    this.router.navigate(['/anime/edit', this.Anime.id]);
   }
-  performDelete(){
-    this.data.deleteAnime(this.Anime.id).subscribe(()=>{
+  performDelete() {
+    this.data.deleteAnime(this.Anime.id).subscribe(() => {
       this.removeanime.emit();
     });
   }
-  
 
-  updateLikecount(count:number){
+  updateLikecount(count: number) {
     this.LikeSubject.next(count);
   }
 
-  updatedisLikecount(count:number){
+  updatedisLikecount(count: number) {
     this.DislikeSubject.next(count);
   }
 
-  toggle(){
-    this.value=!this.value;
+  toggle() {
+    this.value = !this.value;
   }
 
-  gotoMovieDetail(){
-    this.router.navigate([`anime/details/${this.Anime.id}`])
+  gotoMovieDetail() {
+    this.router.navigate([`anime/details/${this.Anime.id}`]);
   }
-  getLabelLanguages(language:string) {
-    
-    
+  getLabelLanguages(language: string) {
     return LANGUAGES.find((data) => data.value === language)?.label;
-   
   }
   openConfirmDialog() {
-    return this.dialog.open(DeleteDialogComponent,
-      {
+    return this.dialog.open(DeleteDialogComponent, {
       maxWidth: '450px',
-      data: { message: 'Are you sure you want to delete this anime?'},
-    }
-    );
+      data: { message: 'Are you sure you want to delete this anime?' },
+    });
   }
 
-    deleteAnime(){
-      this.openConfirmDialog()
-        .afterClosed()
-        .subscribe((confirmed: boolean) => {
-          if (confirmed) {
-            this.performDelete();
-          }
-        });
+  deleteAnime() {
+    this.openConfirmDialog()
+      .afterClosed()
+      .subscribe((confirmed: boolean) => {
+        if (confirmed) {
+          this.performDelete();
         }
-   }
-
+      });
+  }
+}
